00e6265f47ccccbab02b1864e541fa05c67f68ec
Isaias G.C <igerardo0808@gmail.com>
2023-11-02 14:46:27 -0600

    Archivos iniciales del proyecto

---
+++ MagicVilla_API/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MagicVilla_API.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
